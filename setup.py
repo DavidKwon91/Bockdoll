5236d63fe17cd8c33dbadef3df37390ffea0a59c
David Kwon <husiew140@gmail.com>
2020-07-24 20:07:25 +0900

    commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#%%
+import os
+import setuptools
+
+
+setuptools.setup(
+    name="utils",
+    author="David Kwon",
+    author_email="husiew140@gmail.com",
+    description="Some utilities to be used",
+)
