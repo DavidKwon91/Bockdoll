dc9c5ee90cf42a8b6850819e6158a22fd9a25a9c
David Kwon <husiew140@gmail.com>
2020-07-24 20:22:51 +0900

    commit

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ import setuptools
 
 setuptools.setup(
     name="bockdoll",
+    version="1.0.0",
     author="David Kwon",
     author_email="husiew140@gmail.com",
     description="Some utilities to be used",
+    packages=setuptools.find_packages(),
 )
